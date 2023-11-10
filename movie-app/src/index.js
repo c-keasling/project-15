6886a26fd656afe7dedfd3084fa2ab251e850185
Cody-Keasling-LAS1DA <132222567+Cody-Keasling-LAS1DA@users.noreply.github.com>
2023-11-09 20:54:07 -0800

    Set correct homepage for GitHub Pages deployment

--- movie-app/src/index.js
+++ movie-app/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App"; // Import your main App component
+import "./index.css"; // Import your index.css file here
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Wrap the App component with BrowserRouter and set the basename
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter basename="/project-15/movie-app">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
